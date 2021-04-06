2315ca33d8b254db8556fa3dd72649b364d423b9
marcos.francisco <marcos.francisco@PC04541.sjc.sp.gov.br>
2021-04-06 16:26:29 -0300

    commit inicial

---
+++ Listagem/src/Lista.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Lista {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		ArrayList<String> gatos = new ArrayList();
+
+		gatos.add("persa");
+		gatos.add("siames");
+		gatos.add("angora");
+		gatos.add("thai");
+
+		for (Object nome : gatos) {
+
+			System.out.println(nome);
+
+
+	}
+
+}
+	
+}
+
+
